033d0b701225c4afd2d1e7d56a769dffa21ed383
Vaclav Honzik <vaclavhonzik98@gmail.com>
2022-09-25 17:55:08 +0200

    gitignore

---
+++ msvc/Init.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <CL/cl.hpp>
+// Initialization module
+
+
--- scripts/create_test_file.py
+++ scripts/create_test_file.py
@@ -12,15 +12,13 @@ batch_size = 10_000
 
 os.makedirs('../data', exist_ok=True)
 
-
-
 file_size_bytes = file_size_mb * 1024 * 1024
 n_doubles = file_size_bytes // 8
 
 # File might be a bit longer but whatever
 n_batches = n_doubles // batch_size + 1
 
-# Create generator fn
+# Create generator fn dict
 random_fns = {
     'gauss': lambda: nprandom.normal(loc=.0, scale=100_000_000, size=batch_size).astype(float),
     'poisson': lambda: nprandom.poisson(lam=1000, size=batch_size).astype(float),
