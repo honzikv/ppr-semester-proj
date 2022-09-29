b1c1efc77dd3f7d284d176ac46e6759f78cf7d1d
Vaclav Honzik <vaclavhonzik98@gmail.com>
2022-09-29 14:40:58 +0200

    nvidia cuda opencl linker

--- msvc/Init.h
+++ msvc/Init.h
@@ -1,5 +1,6 @@
 #pragma once
-#include <CL/cl.hpp>
-// Initialization module
+
+
+
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "../msvc/argParsing.h"
+#include <CL/cl.h>
 
 int wmain(int argc, wchar_t** argv) {
-	parseArgs(argc, argv);
+	// parseArgs(argc, argv);
+
+
 }
