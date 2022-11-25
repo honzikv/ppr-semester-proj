8bad9b5692cec486e2257b1d1cd83d92e1bba28f
vhonzik <vaclavhonzik98@gmail.com>
2022-11-25 21:45:40 +0100

    it LOGGERS

--- clSandbox/main.cpp
+++ clSandbox/main.cpp
@@ -1,37 +1,22 @@
 #include <iostream>
-#include <vector>
 #include <string>
-#include <numeric>
+#include <cstdio>
+#include <ctime>
 
+// Get current date/time, format is YYYY-MM-DD.HH:mm:ss
+const std::string currentDateTime() {
+    time_t now = time(0);
+    struct tm tstruct{};
+    char buf[80];
+    localtime_s(&tstruct, &now);
+    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+    // for more information about date/time format
+    strftime(buf, sizeof(buf), "%X", &tstruct);
+
+    return buf;
+}
 
 int main() {
-    auto vector = std::vector<size_t>(9);
-    vector[0] = 1;
-//    vector[1] = 4;
-//    vector[2] = 2;
-//    vector[3] = 3;
-//    vector[4] = 5;
-//    vector[5] = 6;
-//    vector[6] = 1;
-//    vector[7] = 2;
-//    vector[8] = 123;
-
-    auto sum = std::accumulate(vector.begin(), vector.end(), 0);
-
-    auto itemsToProcess = vector.size();
-    while (true) {
-        auto nPairs = itemsToProcess / 2 + itemsToProcess % 2;
-        for (auto i = 0; i < nPairs; i += 1) {
-            vector[i] = i * 2 + 1 < itemsToProcess ? vector[i * 2] + vector[i * 2 + 1] : vector[i * 2];
-        }
-
-        if (nPairs == itemsToProcess) {
-            break;
-        }
-
-        itemsToProcess = nPairs;
-    }
-
-    std::cout << vector[0] << std::endl;
-    std::cout << "sum: " << sum << std::endl;
+    std::cout << "currentDateTime()=" << currentDateTime() << std::endl;
+    getchar();  // wait for keyboard input
 }
---
+++ msvc/src/Logging.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+#include <mutex>
+#include <ostream>
+#include <vector>
+#include <chrono>
+#include <ctime>
+#include <array>
+
+// Simple logging module
+
+auto mutex = std::mutex{};
+const auto TIME_FORMAT = "%Y-%m-%d %H:%M:%S";
+const auto MS_FORMAT = ".%03d";
+
+enum LogType {
+	INFO = 0,
+	DEBUG = 1,
+	CRITICAL = 2
+};
+
+const auto LOG_TYPE_LUT = std::vector<std::string>{"(Info)", "(Debug)", "(Error)"};
+
+inline auto getCurrentTimeAsStr() {
+	auto now = time(nullptr);
+	auto timeStruct = tm{};
+	auto buf = std::array<char, 80>();
+
+	// I really care about return values
+	auto _ = localtime_s(&timeStruct, &now);
+	auto __ = strftime(buf.data(), sizeof(buf), "%X", &timeStruct);
+
+	return std::string(buf.data());
+}
+
+inline void log(const LogType logType, const std::string& message) {
+	const auto lock = std::scoped_lock(mutex);
+	std::cout << "[" << getCurrentTimeAsStr() << "] " << LOG_TYPE_LUT[logType] << " " << message << std::endl;
+}
+
